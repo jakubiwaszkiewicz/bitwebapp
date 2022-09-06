a6762b54b7e3f412ed1202b3a60593c520948b6b
augustanol <266454@student.pwr.edu.pl>
2022-09-06 20:54:44 +0200

    navbaaar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import NavBar from "./Components/NavBar"
+import HomePage from "./Components/HomePage";
+import TurniejePage from "./Components/TurniejePage";
+import GameJamPage from "./Components/GameJamPage";
+import AtrakcjePage from "./Components/AtrakcjePage";
+import HarmonogramPage from "./Components/HarmonogramPage";
+import ONasPage from "./Components/ONasPage";
+import PartnerzyPage from "./Components/PartnerzyPage";
+import GaleriaPage from "./Components/GaleriaPage";
+
 function App() {
   return (
-    <div className="app bg-black text-white text-center">
-      chuj
+    <div className="app">
+      <NavBar/>
+      <HomePage/>
+      <TurniejePage/>
+      <GameJamPage/>
+      <AtrakcjePage/>
+      <HarmonogramPage/>
+      <ONasPage/>
+      <PartnerzyPage/>
+      <GaleriaPage/>
     </div>
   );
 }
