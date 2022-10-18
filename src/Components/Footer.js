249bcbd15052e8e6b566b38b2f6d2b98de6a6cb5
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2022-10-18 17:38:20 +0200

    ja pierdole ale siedzi

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,9 @@ import "./styles/footer.css"
 export default function Footer () {
     return (
         <section className="footer">
-            <></>
+            <p>
+                BEST Wrocław © 2022 Jakub Iwaszkiewicz i młodzierzy
+            </p>
         </section>
     )
 }
