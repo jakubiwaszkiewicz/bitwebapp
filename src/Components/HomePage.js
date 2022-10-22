fc29a4bea46276864b5c36b892e65c8085adde4e
augustanol <266454@student.pwr.edu.pl>
2022-10-22 18:27:38 +0200

    harmonogram ładniejszy, przycisk na homepage tez

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -9,7 +9,7 @@ export default function HomePage () {
           <span className="blueNeonText NeonText">IT </span>
           <span className="roseNeonText2 NeonText">FESTIVAL</span>
         </div>
-        <div className="text">8 - 11.12.2022 jakiś budynek</div>
+        <div className="text">8 - 11.12.2022</div>
         <div><button className="home-neon-button"><a href="https://www.facebook.com/BITFestival/">Dołącz do wydarzenia</a></button></div>
       </div>
     </section>
