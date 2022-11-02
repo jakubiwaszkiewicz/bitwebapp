6813f7e4ede26d0c57626cd653e46f88544f620a
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2022-11-02 15:49:47 +0100

    structure of webapp works #2

--- src/Components/data.js
+++ src/Components/data.js
@@ -22,13 +22,6 @@ export default [
   },
   {
     id: 3,
-    description: "chuj",
-    img: "https://live.staticflickr.com/65535/52395797696_52ba5fb35d_w.jpg",
-    alt: "company 1",
-    link: "https://www.google.pl/"/*link do formsa do zapisów*/,
-  },
-  {
-    id: 4,
     description: "Federer and Novak Djokovic have won the second most Grand Slam tournament titles," +
         " trailing only Rafael Nadal. He is the first men's singles player to have reached" +
         " ten consecutive Grand Slam tournament finals and a total of 31 Grand Slam finals. He has earned a men's" +
@@ -38,7 +31,7 @@ export default [
     link: "https://www.google.pl/"/*link do formsa do zapisów*/,
   },
   {
-    id: 5,
+    id: 4,
     description: "Federer and Novak Djokovic have won the second most Grand Slam tournament titles, trailing only Rafael Nadal. He is the first men's singles player to have reached ten consecutive Grand Slam tournament finals and a total of 31 Grand Slam finals. He has earned a men's doubles gold medal, and a men's single silver medal at the Olympics in 2008 and 2012,Federer and Novak Djokovic have won the second most Grand Slam tournament titles, trailing only Rafael Nadal. He is the first men's singles player to have reached ten consecutive Grand Slam tournament finals and a total of 31 Grand Slam finals. He has earned a men's doubles gold medal, and a men's single silver medal at the Olympics in 2008 and 2012,Federer and Novak Djokovic have won the second most Grand Slam tournament titles, trailing only Rafael Nadal. He is the first men's singles player to have reached ten consecutive Grand Slam tournament finals and a total of 31 Grand Slam finals. He has earned a men's doubles gold medal, and a men's single silver medal at the Olympics in 2008 and 2012,",
     img: "https://live.staticflickr.com/65535/52395797696_52ba5fb35d_w.jpg",
     alt: "company 1",
